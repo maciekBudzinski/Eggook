535d871d8d8ffc57a5df344d7294dd78034b2a43
Grzegorz Długokęcki <grzesiek.dlugokecki@gmail.com>
2018-08-31 08:36:02 +0200

    notifications

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Picker, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
+import { Notifications } from 'expo';
 import { BOIL_TYPES } from '../../config/constans';
 import { styles } from './style';
 
 let timer;
+let testNumber = 1;
+let localNotification = {
+  title: '',
+};
 
 class Main extends Component {
+  componentDidMount() {
+    const { timeLeft } = this.props;
+    localNotification = {
+      title: `${testNumber}`,
+      body: '', // (string) — body text of the notification.
+      ios: {
+        // (optional) (object) — notification configuration specific to iOS.
+        sound: true, // (optional) (boolean) — if true, play a sound. Default: false.
+      },
+      // (optional) (object) — notification configuration specific to Android.
+      android: {
+        sound: true, // (optional) (boolean) — if true, play a sound. Default: false.
+        // icon (optional) (string) — URL of icon to display in notification drawer.
+        // color (optional) (string) — color of the notification icon in notification drawer.
+        priority: 'high', // (optional) (min | low | high | max) — android may present notifications according to the priority, for example a high priority notification will likely to be shown as a heads-up notification.
+        sticky: true, // (optional) (boolean) — if true, the notification will be sticky and not dismissable by user. The notification must be programmatically dismissed. Default: false.
+        vibrate: true, // (optional) (boolean or array) — if true, vibrate the device. An array can be supplied to specify the vibration pattern, e.g. - [ 0, 500 ].
+        // link (optional) (string) — external link to open when notification is selected.
+      },
+    };
+    const t = new Date();
+    t.setSeconds(t.getSeconds() + 1);
+    const schedulingOptions = {
+      time: t, // (date or number) — A Date object representing when to fire the notification or a number in Unix epoch time. Example: (new Date()).getTime() + 1000 is one second from now.
+    };
+    Notifications.scheduleLocalNotificationAsync(localNotification, schedulingOptions);
+  }
+
   handleStartStopPress = () => {
     const { isOn, startTimer, stopTimer, timerTick } = this.props;
 
@@ -25,6 +58,10 @@ class Main extends Component {
   };
 
   render() {
+    testNumber += 1;
+    if (localNotification.title !== undefined) {
+      localNotification.title = `${testNumber}`;
+    }
     const { boilType, isOn, timeLeft } = this.props;
 
     const rawMinutes = Math.floor(timeLeft / 60);
